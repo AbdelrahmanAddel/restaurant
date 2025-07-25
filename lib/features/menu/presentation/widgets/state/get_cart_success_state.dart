a5237ef8a65dcbf4668ce3dfe6c34beff10262cd
Abdelrahman <Aa5282243@gmail.com>
2025-07-25 11:45:57 +0300

    Enhance GetCartSuccessState widget to display total price and improve layout
    - Added total price display for items in the cart.
    - Refactored layout to use a Column widget for better organization.
    - Integrated vertical spacing and adjusted the RefreshIndicator for improved user experience.
    - Introduced a private method to calculate the total price of items in the cart.

--- lib/features/menu/presentation/widgets/state/get_cart_success_state.dart
+++ lib/features/menu/presentation/widgets/state/get_cart_success_state.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intern_restaurant/core/helpers/spacer.dart';
 import 'package:intern_restaurant/di/app_dependency_injection.dart';
 import 'package:intern_restaurant/features/menu/data/models/menu_model.dart';
 import 'package:intern_restaurant/features/menu/presentation/cubit/menu_cubit.dart';
@@ -12,23 +14,42 @@ class GetCartSuccessState extends StatelessWidget {
   Widget build(BuildContext context) {
     return cart.isEmpty
         ? const NoItemInCart()
-        : RefreshIndicator(
-            onRefresh: () async {
-              await getIt<MenuCubit>().getCard();
-            },
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.7,
-                mainAxisSpacing: 20,
-                crossAxisSpacing: 20,
+        : Column(
+            children: [
+              Text(
+                'Total Price: ${_calculateTotalPrice(cart)} \$',
+                style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
               ),
-              shrinkWrap: true,
-              itemCount: cart.length,
-              itemBuilder: (context, index) {
-                return CustomCard(menu: cart[index], isAddedToCard: true);
-              },
-            ),
+              verticalSpace(15),
+              Expanded(
+                child: RefreshIndicator(
+                  onRefresh: () async {
+                    await getIt<MenuCubit>().getCard();
+                  },
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 0.7,
+                          mainAxisSpacing: 20,
+                          crossAxisSpacing: 20,
+                        ),
+                    shrinkWrap: true,
+                    itemCount: cart.length,
+                    itemBuilder: (context, index) {
+                      return CustomCard(menu: cart[index], isAddedToCard: true);
+                    },
+                  ),
+                ),
+              ),
+            ],
           );
+  }
+
+  double _calculateTotalPrice(List<MenuModel> cart) {
+    double total = 0;
+    total = cart.fold(0, (sum, item) => sum + (item.price ?? 0));
+
+    return total;
   }
 }
